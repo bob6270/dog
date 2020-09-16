da7fb477632506fea68d9890cfbc3bfb75effe87
bob6270 <70320694+bob6270@users.noreply.github.com>
2020-09-15 21:05:06 -0700

    first commit

---
+++ men/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Display(models.Model):
+    title = models.CharField(max_length=60)
+    desc = models.CharField(max_length=60)
+    date = models.DateField(auto_now=False)
+    url = models.URLField(blank=True)
+    imag = models.ImageField(upload_to='men/images/')
+
+class Display2(models.Model):
+    title = models.CharField(max_length=60)
+    desc = models.CharField(max_length=60)
+    date = models.DateField(auto_now=False)
+    url = models.URLField(blank=True)
+    imag = models.ImageField(upload_to='men/images/')
+
+class Display3(models.Model):
+    title = models.CharField(max_length=60)
+    desc = models.CharField(max_length=60)
+    date = models.DateField(auto_now=False)
+    url = models.URLField(blank=True)
+    imag = models.ImageField(upload_to='men/images/')
---
+++ men/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Display
+from .models import Display2
+from .models import Display3
+# Create your views here.
+def home(request):
+    arg = Display.objects.all()
+    return render(request, 'men/home.html',{'arg':arg})
+def page2(request):
+    arg = Display2.objects.all()
+
+    return render(request, 'men/page2.html',{ 'arg':arg })
+def page3(request):
+    arg = Display3.objects.all()
+    return render(request, 'men/page3.html',{ 'arg':arg })
